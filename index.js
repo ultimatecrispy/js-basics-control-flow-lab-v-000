b0936a02151098b9bf732c8618e8db0618372204
Tom Su <ultimatecrispy@gmail.com>
2017-11-04 22:17:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return  'This one is on me!';
+  } else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city == "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.';
+      break;
+    case "not as generous":
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
+}

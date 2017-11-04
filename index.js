491265318de5d128f0c040b490c5b585ee8f3ee3
Tom Su <ultimatecrispy@gmail.com>
2017-11-04 22:06:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return  'This one is on me!';
-  } else if (distance > 2000) {
+  } else if (distance > 2000 && distance < 2500) {
     return 'I will gladly take your thirty bucks.';
   } else {
     return 'No can do.';

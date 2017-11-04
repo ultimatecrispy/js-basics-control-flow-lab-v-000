3bb78738b5594b7beb6a244ce4b115d0fa66424e
Tom Su <ultimatecrispy@gmail.com>
2017-11-04 22:12:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function ternaryCheckCity(city) {
   }
 }
 
-function switchOnCharmFromTip() {
+function switchOnCharmFromTip(tip) {
   switch (tip) {
     case "generous":
       return 'Thank you so much.';

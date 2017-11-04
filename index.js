6c06ab211c4ba25cd1191455c241e6bd28ba70ab
Tom Su <ultimatecrispy@gmail.com>
2017-11-04 22:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return  'This one is on me!';
   } else if (distance > 2000) {
-    return 'Hi';
+    return 'I will gladly take your thirty bucks.';
   }
 }

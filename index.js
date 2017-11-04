c33828265548b9c2415bec9bebd9613393e68df6
Tom Su <ultimatecrispy@gmail.com>
2017-11-04 22:15:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function switchOnCharmFromTip(tip) {
     case "generous":
       return 'Thank you so much.';
       break;
-    case !"generous":
+    case "generous":
       return 'Thank you';
       break;
     default:
